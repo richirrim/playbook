db7ae157dcc26512ebf4d85565d4379370d53626
richirrim <ricardoortega.dev@gmail.com>
2022-04-15 13:52:03 -0500

    Crea practica dos de la sección de Clases y Objetos.

---
+++ weekly_mission_2/3_clases_y_objetos/clases_2.js
@@ -0,0 +1,16 @@
+/**
+ * CLASES Y OBJETOS
+ * ----------------------
+ *  
+ * Ejemplo 2: Crear un objeto a partir de una clase.
+ *  */
+
+
+class Ninja {
+}
+
+const itachi = new Ninja()
+const obito = new Ninja()
+
+console.log(itachi)
+console.log(obito)
